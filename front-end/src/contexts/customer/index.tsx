3c6158e443a314c3ce141b6c1e01dae86189a9cf
Emerson Gonçalves dos Santos <emer_gs@hotmail.com>
2023-02-03 05:02:06 +0000

    feat:contrução do context de customer

--- front-end/src/contexts/customer/index.tsx
+++ front-end/src/contexts/customer/index.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { IChildren } from "..";
 import { toast } from "react-toastify"
 import { useNavigate } from "react-router-dom";
@@ -6,6 +6,13 @@ import api from "../../services/api";
 
 interface ICustomerContext {
   customerLogin: (data: ICustomerLogin) => void,
+  deleteCustomerStorage: ()=>void,
+  loadUser: ()=>void,
+  updateCustomer: ()=>void,
+  retriveProfile:()=>void,
+  customer: ICustomer,
+  loading: boolean,
+
 }
 
 export interface ICustomerLogin {
@@ -22,6 +29,13 @@ interface ICustomer {
   createdAt: Date
 }
 
+interface ICustomerUpdate {
+  name: string,
+  email: string,
+  password: string,
+  phone: string,
+}
+
 
 
 export const CustomerContext = createContext<ICustomerContext>({} as ICustomerContext);
@@ -30,8 +44,33 @@ const CustomerProvider = ({ children }: IChildren) => {
 
   //states
   const [customer, setCustomer] = useState<ICustomer>({} as ICustomer)
+  const [count, setCount] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(true)
 
   const navigate = useNavigate()
+
+  useEffect(()=>{
+
+  const loadUser = async ()=>{
+    const token = JSON.parse (localStorage.getItem('@appDesafioFullStackM6TOKEN') || '{}')
+    if(token){
+      
+      try {
+        api.defaults.headers.common.authorization = `Bearer ${token}`
+        const {data} = await api.get('/profile')
+        setCustomer(data)
+      } 
+      catch (error) {
+        console.error(error);
+        localStorage.removeItem('@appDesafioFullStackM6TOKEN')
+        localStorage.removeItem('@appDesafioFullStackM6USERID')
+      }
+    }
+    setLoading(false)
+  }
+
+  loadUser()
+  },[count])
 
   const customerLogin = async (data: ICustomerLogin) => {
     function validateLogin() {
@@ -48,6 +87,43 @@ const CustomerProvider = ({ children }: IChildren) => {
 
     const request = await api.post('/login', data)
     request.status === 200 ? validateLogin() : doNotValidateLogin()
+  }
+
+  const deleteCustomerStorage = ()=>{
+    navigate('../login', {replace:true})
+    localStorage.removeItem('@appDesafioFullStackM6TOKEN')
+    localStorage.removeItem('@appDesafioFullStackM6USERID')
+  }
+
+  const addCount = ()=>{
+    setCount(count+1)
+  }  
+
+  const updateCustomer = async (data:ICustomerUpdate)=>{
+    const token = JSON.parse(localStorage.getItem('@appDesafioFullStackM6TOKEN') || '{}')
+      
+    try {
+      api.defaults.headers.common.authorization = `Bearer ${token}`
+      const {data} = await api.get('/profile')
+      await api.patch(`/customer/profile`,data)
+      setCount(count+1)
+      toast.success('Dados editados com sucesso')
+    } 
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  const retriveProfile = async()=>{
+    const token = JSON.parse(localStorage.getItem('@appDesafioFullStackM6TOKEN') || '{}')
+    try {
+      api.defaults.headers.common.authorization = `Bearer ${token}`
+      await api.get(`/profile`)
+      setCount(count+1)
+    } 
+    catch (error) {
+      console.error(error);
+    }
   }
 
   return (
